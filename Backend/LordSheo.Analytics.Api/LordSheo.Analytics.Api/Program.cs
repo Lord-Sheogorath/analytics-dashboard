e1f67dabe4341f47cd6aec5de057d95cb5ece204
Jack Smith-Hocking <hockingjack818@gmail.com>
2025-07-26 18:00:13 +0930

    Added test data generation to backend

--- Backend/LordSheo.Analytics.Api/LordSheo.Analytics.Api/Program.cs
+++ Backend/LordSheo.Analytics.Api/LordSheo.Analytics.Api/Program.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using LordSheo.Analytics.Api.Data;
+using LordSheo.Analytics.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -40,4 +42,45 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<AnalyticsDbContext>();
+
+	// Optional: ensure DB created & migrations applied
+	db.Database.Migrate();
+
+	// Only seed if no events exist
+	if (!db.AnalyticsEvents.Any())
+	{
+		SeedTestEvents(db, 1000);  // generate 100 test events
+	}
+}
+
 app.Run();
+
+void SeedTestEvents(AnalyticsDbContext context, int count)
+{
+	var rnd = new Random();
+	var eventNames = new[] { "Login", "Logout", "PageView", "Click", "Purchase" };
+
+	var events = new List<AnalyticsEvent>();
+
+	for (int i = 0; i < count; i++)
+	{
+		var e = new AnalyticsEvent
+		{
+			EventName = eventNames[rnd.Next(eventNames.Length)],
+			UserId = Guid.NewGuid().ToString(),
+			Timestamp = DateTime.UtcNow.AddMinutes(-rnd.Next(0, 60 * 24 * 7)), // last 7 days
+			MetadataJson = JsonSerializer.Serialize(new Dictionary<string, string>
+			{
+				{ "Detail", "Test event #" + i },
+				{ "RandomValue", rnd.Next(1000).ToString() }
+			})
+		};
+		events.Add(e);
+	}
+
+	context.AnalyticsEvents.AddRange(events);
+	context.SaveChanges();
+}
